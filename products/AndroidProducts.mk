2c9ec9d1abedecfd8828061af348c5d15e8f29ab
Jeremy Blankenship <jerbla88@gmail.com>
2012-04-18 22:23:31 -0700

    Fixing git commit to match current Do note merge yet. Change-Id: I38a97409568c0c50d1899de5255c7465edcd9c0c

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,6 +4,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/a500.mk \
+    $(LOCAL_DIR)/epicmtd.mk \
     $(LOCAL_DIR)/captivatemtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
