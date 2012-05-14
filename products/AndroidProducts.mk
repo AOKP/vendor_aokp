68b88e62e3939e2369e4a956dd71d4934ffeb356
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-05-14 01:16:52 -0700

    vendor/aokp: add hercules - DO NOT MERGE
    Change-Id: I19e601e005dc55c15a73706587de33cc4bbcc97c

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -9,6 +9,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/espressowifi.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
+    $(LOCAL_DIR)/hercules.mk \
     $(LOCAL_DIR)/inc.mk \
     $(LOCAL_DIR)/otter.mk \
     $(LOCAL_DIR)/p1c.mk \
