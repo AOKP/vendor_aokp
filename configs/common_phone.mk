0aae1061bdb8c7a573d0f984aa9a1b3dafdaaf77
mrbirdman <thebirdman.08@gmail.com>
2012-07-16 18:28:42 -0700

    remove novalauncher being copied for now

--- configs/common_phone.mk
+++ configs/common_phone.mk
@@ -1,9 +1,5 @@
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
- BUILD_PREBUILT += \
-    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
-
-
 # Inherit drm blobs
 -include vendor/aokp/configs/common_drm_phone.mk
 
