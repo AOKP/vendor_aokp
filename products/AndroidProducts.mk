ee967d24b11ec6bc63da16cecc4609a9fb2e570d
Stevespear426 <stephen.k.spear@gmail.com>
2012-07-30 02:25:31 -0400

    Add Fascinate, Mesmerize, Showcase
    Change-Id: Ib4125d156b75643f9b3e6eb82e7c9e1b9bc089ac
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,8 +2,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/d2tmo.mk \
+    $(LOCAL_DIR)/fascinatemtd.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/maguro.mk \
+    $(LOCAL_DIR)/mezmerizemtd.mk \
+    $(LOCAL_DIR)/showcasemtd.mk \
     $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/torospr.mk \
