b337465832c5c1558060f29e25e6a5588b9f2ab5
mrbirdman <thebirdman.08@gmail.com>
2012-08-01 03:21:18 -0700

    add espressowifi
    Change-Id: If039cf292ae91be6d708f082c02668533ac11a9a

---
+++ products/espressowifi.mk
@@ -0,0 +1,19 @@
+# Inherit device configuration
+$(call inherit-product, device/samsung/espressowifi/full_espressowifi.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := espressowifi
+PRODUCT_NAME := aokp_espressowifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-P3113
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=espressowifiue BUILD_FINGERPRINT=samsung/espressowifiue/espressowifi:4.0.3/IML74K/P3113UEALD3:user/release-keys PRIVATE_BUILD_DESC="espressowifiue-user 4.0.3 IML74K P3113UEALD3 release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := espressowifi
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,9 @@
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
-add_lunch_combo aokp_maguro-userdebug
+add_lunch_combo aokp_espressowifi-userdebug
 add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_stingray-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
