e3badba918265fdc0c03a42903f0ca8b7e11ce01
Jason Stewart <sixstringsg@verizon.net>
2012-08-04 05:30:31 +0100

    vendor_aokp - add in aries GSM devices
    This includes the Captivate, the GalaxyS i9000, and the Vibrant
    
    Change-Id: Ifea99fa80f07bef8f8ef3291ef92b1c91b0cdd91

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,12 @@
+add_lunch_combo aokp_captivatemtd-userdebug
 add_lunch_combo aokp_crespo-userdebug
 add_lunch_combo aokp_crespo4g-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
+add_lunch_combo aokp_galaxysmtd-userdebug
 add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_stingray-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_toroplus-userdebug
+add_lunch_combo aokp_vibrantmtd-userdebug
 add_lunch_combo aokp_wingray-userdebug
