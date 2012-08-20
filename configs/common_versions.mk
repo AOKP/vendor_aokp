b012bc9fdfd4755021d3ee282c38a02e4bcc3b9d
Roman Birg <romanbirg@gmail.com>
2012-08-20 21:59:02 +0100

    Revert "use current date as modversion"
    This reverts commit 81a12bf7714cdf253f84db900d378a282dcd0d5a

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,10 +1,10 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03C BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell date +%h-%d-%y)
-
+# Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=$(TARGET_PRODUCT)_jb-$(DATE)
+    ro.rommanager.developerid=teamkang \
+    ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
