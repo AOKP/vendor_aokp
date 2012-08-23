c1ad19e453f4c7b3532b8d904362fbb755fe6b3c
dagr8 <ren.o.custom@gmail.com>
2012-08-22 20:04:46 -0400

    overlay and stuff for quincyatt
    Change-Id: Ic16ba22a17330c93bc54afcf002e99dd711956f2

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,6 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/p5113.mk \
     $(LOCAL_DIR)/p3113.mk \
+    $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/torospr.mk \
