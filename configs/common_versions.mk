ddf9727f95b22d5e2a0ddcd18c6c6290bb6b4695
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-08-23 17:57:45 -0700

    AOKP: restore back to nightly status
    Change-Id: I54dca65c99ded1eaf3e04d568a77afc4b8b35db9

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -4,7 +4,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=relea
 DATE = $(shell date +%h-%d-%y)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1
+    ro.aokp.version=$(TARGET_PRODUCT)_jb-$(DATE)
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
