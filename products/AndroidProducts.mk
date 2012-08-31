622b392f4287be5d91fed73750610a8f0944f05e
dagr8 <ren.o.custom@gmail.com>
2012-08-31 02:03:21 -0400

    overlay and stuff quincyatt
    Change-Id: I4ec47b2921fff8903c2cbf7e163ec1b2cdee531a
    
    Conflicts:
    
    	products/AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -13,9 +13,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/p3100.mk \
     $(LOCAL_DIR)/p3110.mk \
     $(LOCAL_DIR)/p3113.mk \
-    $(LOCAL_DIR)/p5100.mk \
-    $(LOCAL_DIR)/p5110.mk \
-    $(LOCAL_DIR)/p5113.mk \
+    $(LOCAL_DIR)/quincyatt.mk \
     $(LOCAL_DIR)/stingray.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/torospr.mk \
