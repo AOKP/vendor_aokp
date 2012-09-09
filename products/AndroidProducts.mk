4317972063d05eca9cc5e78ecb1fa778b6323203
rohan <rohanmathur34@gmail.com>
2012-09-09 08:47:25 -0400

    Add more AT&T APNs
    Added the AT&T LTE APN and StraightTalk AT&T APN
    Signed-off-by: rohan <rohanmathur34@gmail.com>
    
    Change-Id: Ie619231b9b4aec5c6c3ec3e9c326dbd9579596c3

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,6 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/crespo.mk \
     $(LOCAL_DIR)/crespo4g.mk \
     $(LOCAL_DIR)/d2tmo.mk \
+    $(LOCAL_DIR)/evita.mk \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/galaxysmtd.mk \
     $(LOCAL_DIR)/fascinatemtd.mk \
