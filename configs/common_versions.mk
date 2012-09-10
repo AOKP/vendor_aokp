bf46bae267b7825616e573a16a2842f671b425f6
KAsp3rd <casper200519@gmail.com>
2012-09-10 14:53:37 -0400

    common_version: Made changes for nightly
    Changes require export AOKP_NIGHTLY to be defined in a build script
    or bash
    
    Signed-off-by: KAsp3rd <casper200519@gmail.com>
    
    Change-Id: I54dca65c99ded1eaf3e04d568a77afc4b8b35db9

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -3,14 +3,25 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=JRO03H BUILD_VERSION_TAGS=relea
 
 DATE = $(shell date +%h-%d-%y)
 
+#Define Nightly Build
+ifdef AOKP_NIGHTLY
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.aokp.version=$(TARGET_PRODUCT)_jb-$(DATE)
+# Goo updater app
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=aokp \
+    ro.goo.board=$(TARGET_PRODUCT)
+    ro.goo.rom=aokp \
+    ro.goo.version=$(DATE)
+else
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.aokp.version=$(TARGET_PRODUCT)_jb-build-1
-
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=aokp \
     ro.goo.rom=aokp \
     ro.goo.version=21
+endif
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
