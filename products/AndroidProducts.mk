f8da50173afcce8e9375d14f1b508cd519d07e11
David Gasaway <ponsasinorem@gmail.com>
2012-10-07 14:12:03 -1000

    products/AndroidProducts.mk only use TARGET_DEVICE
    Only grab the TARGET_DEVICE's.mk instead of every device.mk when compiling a single device.
    
    Patch Set 1: Initial commit
    Patch Set 2: Added check for blank TARGET_PRODUCT
    
    Change-Id: I0c838dfb22c688ec77e1a18548698f595d264c4d

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,28 +1,33 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/captivatemtd.mk \
-    $(LOCAL_DIR)/crespo.mk \
-    $(LOCAL_DIR)/crespo4g.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
-    $(LOCAL_DIR)/d2usc.mk \
-    $(LOCAL_DIR)/d2vzw.mk \
-    $(LOCAL_DIR)/evita.mk \
-    $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/fascinatemtd.mk \
-    $(LOCAL_DIR)/galaxysmtd.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/hercules.mk \
-    $(LOCAL_DIR)/i9300.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/n7000.mk \
-    $(LOCAL_DIR)/p3100.mk \
-    $(LOCAL_DIR)/p3110.mk \
-    $(LOCAL_DIR)/p3113.mk \
-    $(LOCAL_DIR)/p5100.mk \
-    $(LOCAL_DIR)/p5110.mk \
-    $(LOCAL_DIR)/p5113.mk \
-    $(LOCAL_DIR)/tf101.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk \
-    $(LOCAL_DIR)/vibrantmtd.mk
+ifneq ($(TARGET_PRODUCT),)
+    PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/captivatemtd.mk \
+        $(LOCAL_DIR)/crespo.mk \
+        $(LOCAL_DIR)/crespo4g.mk \
+        $(LOCAL_DIR)/d2tmo.mk \
+        $(LOCAL_DIR)/d2usc.mk \
+        $(LOCAL_DIR)/d2vzw.mk \
+        $(LOCAL_DIR)/evita.mk \
+        $(LOCAL_DIR)/galaxysmtd.mk \
+        $(LOCAL_DIR)/fascinatemtd.mk \
+        $(LOCAL_DIR)/galaxysmtd.mk \
+        $(LOCAL_DIR)/grouper.mk \
+        $(LOCAL_DIR)/hercules.mk \
+        $(LOCAL_DIR)/i9300.mk \
+        $(LOCAL_DIR)/i9100.mk \
+        $(LOCAL_DIR)/i9100g.mk \
+        $(LOCAL_DIR)/maguro.mk \
+        $(LOCAL_DIR)/n7000.mk \
+        $(LOCAL_DIR)/p3100.mk \
+        $(LOCAL_DIR)/p3110.mk \
+        $(LOCAL_DIR)/p3113.mk \
+        $(LOCAL_DIR)/p5100.mk \
+        $(LOCAL_DIR)/p5110.mk \
+        $(LOCAL_DIR)/p5113.mk \
+        $(LOCAL_DIR)/tf101.mk \
+        $(LOCAL_DIR)/toro.mk \
+        $(LOCAL_DIR)/toroplus.mk \
+        $(LOCAL_DIR)/vibrantmtd.mk
+else
+    PRODUCT_MAKEFILES := \
+        $(LOCAL_DIR)/$(shell echo $(TARGET_PRODUCT) | sed s/aokp_//).mk
+endif
