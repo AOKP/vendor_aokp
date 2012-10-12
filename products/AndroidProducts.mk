55bb1c1af12263d9742976984f859e94f3460b2a
Lalit Maganti <lalitmaganti@gmail.com>
2012-10-12 13:38:23 -0700

    vendor: add Xperia T
    Huge thanks to Kali-, codeworkx and Jerpelea for their awesome work on the device.

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -15,6 +15,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/i9100.mk \
     $(LOCAL_DIR)/i9100g.mk \
     $(LOCAL_DIR)/maguro.mk \
+    $(LOCAL_DIR)/mint.mk \
     $(LOCAL_DIR)/n7000.mk \
     $(LOCAL_DIR)/p3100.mk \
     $(LOCAL_DIR)/p3110.mk \
---
+++ products/mint.mk
@@ -0,0 +1,45 @@
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Inherit AOSP device configuration for anzu
+$(call inherit-product-if-exists, device/sony/mint/full_mint.mk)
+
+# mint overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/mint
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_mint
+PRODUCT_BRAND := Sony
+PRODUCT_DEVICE := mint
+PRODUCT_MODEL := Xperia T
+PRODUCT_MANUFACTURER := Sony
+
+#skip asserts for now
+TARGET_OTA_ASSERT_SKIP := true
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT30p_1266-0471 BUILD_FINGERPRINT=Sony/LT30p_1266-0471/LT30p:4.0.4/7.0.A.1.303/0Pr_xw:user/release-keys PRIVATE_BUILD_DESC="LT30p-user 4.0.4 7.0.A.1.303 0Pr_xw test-keys"
+
+# Camera
+PRODUCT_PACKAGES += \
+    Camera \
+    Mms \
+    Music
+
+# Copy anzu specific prebuilt files
+PRODUCT_PACKAGES += \
+    bootanimation_720_1280 \
+    ThinkFree  
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
+
+# Hardware Info
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.device.cpu=Snapdragon_S4_Krait \
+    ro.device.front_cam=1.3MP \
+    ro.device.gpu=Adreno_225 \
+    ro.device.rear_cam=13MP \
+    ro.device.screen_res=720x1280_Pixels
