d874d678eac71a502fcea8e7a65cbd4b5af9b150
David Gasaway <ponsasinorem@gmail.com>
2012-10-14 19:52:25 +0100

    Revert "Use prebuilt su and supersu.apk"
    This reverts commit 117c6106ba21af025177c2810cd5635dce82c4ba
    
    Rebased
    
    Change-Id: Ia38ea015259cad279d4c1922b249f7046fd58d10

--- configs/common.mk
+++ configs/common.mk
@@ -16,13 +16,10 @@ PRODUCT_PACKAGES += \
     NoiseField \
     PhaseBeam \
     ROMControl \
-    SuperSU \
+    su \
+    Superuser \
     SwagPapers \
     UnicornPorn
-
-# Use prebuilt su until fixed when built
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/xbin/su:system/xbin/su
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
@@ -37,38 +34,51 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/lib/libmicrobes_jni.so \
-    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
     vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
 
 # init.d
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/init.d/00start:system/etc/init.d/00start \
-    vendor/aokp/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-    vendor/aokp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
-    vendor/aokp/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/aokp/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
+    vendor/aokp/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
+    vendor/aokp/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+    vendor/aokp/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/aokp/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
+    vendor/aokp/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
+    vendor/aokp/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
+    vendor/aokp/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
+    vendor/aokp/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
+    vendor/aokp/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
+    vendor/aokp/prebuilt/common/etc/init_trigger.disabled:system/etc/init_trigger.disabled \
+    vendor/aokp/prebuilt/common/etc/liberty.bsh:system/etc/liberty.bsh \
+    vendor/aokp/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf
+
+# Cron
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/common/etc/cron/cron.conf:system/etc/cron/cron.conf \
+    vendor/aokp/prebuilt/common/etc/cron/cron.hourly/00drop_caches:system/etc/cron/cron.hourly/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.daily/00drop_caches:system/etc/cron/cron.daily/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.weekly/00drop_caches:system/etc/cron/cron.weekly/00drop_caches \
+    vendor/aokp/prebuilt/common/etc/cron/cron.hourly/01clear_cache:system/etc/cron/cron.hourly/01clear_cache \
+    vendor/aokp/prebuilt/common/etc/cron/cron.daily/01clear_cache:system/etc/cron/cron.daily/01clear_cache \
+    vendor/aokp/prebuilt/common/etc/cron/cron.weekly/01clear_cache:system/etc/cron/cron.weekly/01clear_cache
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
+# Live Wallpapers for all
+PRODUCT_PACKAGES += \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    VisualizationWallpapers \
+    librs_jni
+
 PRODUCT_PACKAGES += \
     openvpn \
     e2fsck \
     mke2fs \
-    tune2fs \
-    libssh \
-    ssh \
-    sshd \
-    sshd-config \
-    ssh-keygen \
-    sftp \
-    scp
-
-# Default ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Scarabaeus.ogg \
-    ro.config.notification_sound=Antimony.ogg \
-    ro.config.alarm_alert=Scandium.ogg
+    tune2fs
 
 PRODUCT_COPY_FILES += packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
