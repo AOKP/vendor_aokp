6361dba9a3710bc0bf07a4238f7908d06a2a7920
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-11-16 23:58:53 -0500

    d2-common: Add overlay for Soft Key Modifications
    Enable soft keys (menu, home, back) for d2 variants.
    
    PS2: Remove d2vzw overlay, add FastCharge toggle to d2-common, and redirect
         overlay for d2vzw to d2-common
    PS3: Readd d2vzw overlay (LTE toggle ;) )
    PS4: Include d2-common and d2vzw in d2vzw.mk
    
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Ib775957f338f7542ab8332382f57bae90a8db1db

--- products/d2vzw.mk
+++ products/d2vzw.mk
@@ -10,7 +10,10 @@ $(call inherit-product, vendor/aokp/configs/common_phone.mk)
 # Inherit common Verizon Wireless Perms and Lib
 $(call inherit-product, vendor/aokp/configs/vzw.mk)
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/d2-common
+# Include d2vzw after to override the things
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/d2vzw
+
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_d2vzw
