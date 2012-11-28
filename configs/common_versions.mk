b47ab8dcd3fae8cef3254c2a788359dde1d85501
BMc08GT <bmc08gt.teamb00sted@gmail.com>
2012-11-27 19:56:13 -0500

    otatools: Consolidate commands into one file
    Merges colors and getdate into one 'commands' file
    
    Also added zip size output to squisher
    
    output will be 'Package complete: 134M  /path/to/zip/aokp_d2vzw_unofficial_Nov-27-12.zip'
    
    Signed-off-by: BMc08GT <bmc08gt.teamb00sted@gmail.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,10 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-DATE = $(shell vendor/aokp/tools/getdate)
+# Import getdate command
+. vendor/aokp/tools/commands
+
+DATE=$GETDATE
 
 ifneq ($(AOKP_BUILD),)
 	# AOKP_BUILD=<goo version int>/<build string>
