04d0c2b5e362b662c48f496e0977316c4c9cea22
rohan <rohanmathur34@gmail.com>
2013-01-19 17:27:43 -0500

    vendor: Add evita (HTC One XL) (2/2)
    Change-Id: Ib935d6caf578333f087e9f6abcef772e7fcc2eda

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,6 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2usc.mk \
     $(LOCAL_DIR)/d2vzw.mk \
+    $(LOCAL_DIR)/evita.mk \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/mako.mk \
