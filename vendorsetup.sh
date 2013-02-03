4eafc2e8b92fe2f61d2700907cce57967a944f0f
rohan <rohanmathur34@gmail.com>
2013-02-03 10:20:06 -0500

    vendor: Add evita (HTC One XL) (2/2)
    Also reorder devices in lunch menu so that grouper is alphabetical again.
    
    Change-Id: Ib935d6caf578333f087e9f6abcef772e7fcc2eda
    
    Conflicts:
    
    	vendorsetup.sh
    
    Signed-off-by: rohan <rohanmathur34@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,10 +3,11 @@ add_lunch_combo aokp_d2att-userdebug
 add_lunch_combo aokp_d2tmo-userdebug
 add_lunch_combo aokp_d2usc-userdebug
 add_lunch_combo aokp_d2vzw-userdebug
+add_lunch_combo aokp_evita-userdebug
+add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_toro-userdebug
 # add_lunch_combo aokp_toroplus-userdebug
-add_lunch_combo aokp_grouper-userdebug
 add_lunch_combo aokp_tilapia-userdebug
