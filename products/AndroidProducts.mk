79dba4e1d38e70e9bc438726cff99a3fedb92ffc
zyr3x <izzzum@gmail.com>
2013-02-17 00:23:51 +0300

    mediapad
    Change-Id: If9201ec485e7830e365116251ff4bf9a3e8f5696

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,17 +1,1 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/a510.mk \
-    $(LOCAL_DIR)/d2att.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
-    $(LOCAL_DIR)/d2usc.mk \
-    $(LOCAL_DIR)/d2vzw.mk \
-    $(LOCAL_DIR)/grouper.mk \
-    $(LOCAL_DIR)/hercules.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/mako.mk \
-    $(LOCAL_DIR)/manta.mk \
-    $(LOCAL_DIR)/t0lte.mk \
-    $(LOCAL_DIR)/tilapia.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/hws7300u.mk
