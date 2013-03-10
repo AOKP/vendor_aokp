244f4bc03f0c66b09db56b0c07f94bccc8ec56ce
ChampionSwimmer <android@championswimmer.tk>
2013-03-10 15:35:00 +0530

    aokp: add Sony Xperia V and Xperia T
    PS4 : added mint to vendorsetup and update overlays
    PS5 : (derp) add mint to AndroidProducts.mk
    PS6 : remove unneeded overlay arrays
    
    Change-Id: I6d037d52b2ad0221ff3f3af87df06ea08bc62435
    Signed-off-by: ChampionSwimmer <android@championswimmer.tk>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -16,6 +16,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/manta.mk \
     $(LOCAL_DIR)/maserati.mk \
+    $(LOCAL_DIR)/mint.mk \
     $(LOCAL_DIR)/p930.mk \
     $(LOCAL_DIR)/solana.mk \
     $(LOCAL_DIR)/spyder.mk \
@@ -25,5 +26,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/tilapia.mk \
     $(LOCAL_DIR)/toro.mk \
     $(LOCAL_DIR)/toroplus.mk \
+    $(LOCAL_DIR)/tsubasa.mk \
     $(LOCAL_DIR)/umtsspyder.mk \
     $(LOCAL_DIR)/vs920.mk
