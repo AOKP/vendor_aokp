7f737f40c54758b06b4cfeba75f934f8f9825aed
Roman Birg <romanbirg@gmail.com>
2013-05-04 15:04:30 -0500

    aokp_m7
    Change-Id: Ic92de429b4ffe516b84d5fd891722c20b137d6f6

---
+++ products/m7.mk
@@ -0,0 +1,24 @@
+# Inherit device configuration
+$(call inherit-product, device/htc/m7/device_m7.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+
+# Device naming
+PRODUCT_DEVICE := m7
+PRODUCT_NAME := aokp_m7
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := HTC One
+PRODUCT_MANUFACTURER := HTC
+
+# Release name
+PRODUCT_RELEASE_NAME := m7
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7/m7:4.1.2/JZO54K/166937.7:user/release-keys" PRIVATE_BUILD_DESC="1.29.651.7 CL166937 release-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
