37db012aa8ecf3e653b8984abfd4dec33cc82f0a
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-05-23 17:16:00 +0200

    Add jfltespr (assumes jfltevzw, jflteusc already added)
    Change-Id: Ib1f311af2c6054693030b15fe3be6d7f2282b914

--- products/jfltespr.mk
+++ products/jfltespr.mk
@@ -3,20 +3,8 @@ $(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/aokp/configs/cdma.mk)
 
-# Enhanced NFC
-# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/aokp/configs/common.mk)
-
-## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jfltespr/jfltespr-vendor.mk)
-
-## common overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay-cdma
-
-# Inherit from jf-common
-$(call inherit-product, device/samsung/jf-common/jf-common.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := aokp_jfltespr
@@ -27,3 +15,6 @@ PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDC:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDC release-keys"
 
+# boot animation 
+PRODUCT_COPY_FILES += \
+       vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
