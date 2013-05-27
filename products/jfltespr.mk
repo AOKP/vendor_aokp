7180a3d28a8f629d05c3f663b8c7d8517a3ab38e
XDA Garwynn <xdagarwynn@gmail.com>
2013-05-27 01:55:39 -0500

    Add jfltespr.
    Device tree can be found at http://github.com/garwynn/android_device_samsung_jfltespr, branch jb-mr1.
    
    At the moment requires CM android_device_samsung_msm8960-common and qcom-common for proper function but will work with Brandon as he will also need this fixed for Verizon.
    
    Change-Id: I777106d7700b66699f9f2796936851ee5e4282c2
    
    Introduce jfltespr (fixed format to match vzw, usc as mentioned by Brandon)

---
+++ products/jfltespr.mk
@@ -0,0 +1,21 @@
+# Inherit AOSP device configuration for jfltespr
+$(call inherit-product, device/samsung/jfltespr/full_jfltespr.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/cdma.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_jfltespr
+PRODUCT_DEVICE := jfltespr
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := SPH-L720
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltespr TARGET_DEVICE=jfltespr BUILD_FINGERPRINT="samsung/jfltespr/jfltespr:4.2.2/JDQ39/L720VPUAMDC:user/release-keys" PRIVATE_BUILD_DESC="jfltespr-user 4.2.2 JDQ39 L720VPUAMDC release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+       vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip 
