7426c885b7ce069934e0b248e40fdee3083bc4fa
MightyBear007@gmail.com <mightybear007@gmail.com>
2013-11-29 17:22:41 +0100

    aokp: add Xperia Z1
    Change-Id: Ifb1f75525cddb2fe18d5d35e3484956fdc72fe0c

---
+++ products/honami.mk
@@ -0,0 +1,21 @@
+# Inherit AOSP device configuration for hammerhead
+$(call inherit-product, device/sony/honami/full_honami.mk)
+
+# Inherit AOKP common bits
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff
+# TODO what to use for Hammerhead?
+#$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Setup device specific product configuration
+PRODUCT_NAME := aokp_honami
+PRODUCT_DEVICE := honami
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia Z1
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903_1276-7948/C6903:4.2.2/14.1.G.1.526/8Xl-jw:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.2.2 14.1.G.1.526 8Xl-jw test-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo aokp_hammerhead-userdebug
+add_lunch_combo aokp_honami-userdebug
 add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
