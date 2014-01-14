04ed0f2637ff023d22d528f3eb1d8688a1d2b756
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-01-14 23:31:35 +0100

    add AOKP KitKat White theme
    Change-Id: Ie733a331ab94c58b8c7cf968a038e8aabffbf48d
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -18,7 +18,8 @@ PRODUCT_PACKAGES += \
     su \
     SwagPapers \
     Torch \
-    UnicornPorn
+    UnicornPorn \
+    KitKatWhite
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
