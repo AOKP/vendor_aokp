e57552b56e6c7c342034f69613ef44aca8caea51
Andrew Warfield <hiasant@gmail.com>
2014-01-24 14:58:29 -0800

    vendor: Unify all GSM m7 builds into just 'm7'
    Change-Id: I152bb29cd6987c3976f4cdba2428071cace461d7

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,10 +27,8 @@ add_lunch_combo aokp_jflteusc-userdebug
 add_lunch_combo aokp_jfltevzw-userdebug
 add_lunch_combo aokp_l900-userdebug
 add_lunch_combo aokp_ls980-userdebug
-add_lunch_combo aokp_m7att-userdebug
+add_lunch_combo aokp_m7-userdebug
 add_lunch_combo aokp_m7spr-userdebug
-add_lunch_combo aokp_m7tmo-userdebug
-add_lunch_combo aokp_m7ul-userdebug
 add_lunch_combo aokp_m7vzw-userdebug
 add_lunch_combo aokp_mint-userdebug
 add_lunch_combo aokp_n1-userdebug
