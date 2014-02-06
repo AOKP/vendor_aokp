d2a99296d6e7b82ad000e16cb24d5fbbef1d3e51
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-02-06 03:40:03 +0100

    d2: Unify
    Change-Id: Id49c24cc8a126ef1f17fe1c30a903984413f3919
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

---
+++ products/d2.mk
@@ -0,0 +1,21 @@
+# Inherit AOSP device configuration for d2
+$(call inherit-product, device/samsung/d2/full_d2.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
+
+# Inherit common d2 overlays
+DEVICE_PACKAGE_OVERLAYS += vendor/aokp/overlay/samsung/d2 \
+                           vendor/aokp/overlay/samsung/msm8960-common \
+                           vendor/aokp/overlay/samsung/qcom-common
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_d2
+PRODUCT_DEVICE := d2
+
+# boot animation
+PRODUCT_COPY_FILES += \
+vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
