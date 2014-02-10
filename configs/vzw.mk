87feaa0a6feef845602dc358bae6b2b901d677ed
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-02-10 23:49:40 +0100

    vzw: Add logic to vzw makefile for TARGET_UNIFIED_DEVICE
    * If target is a unified device, move the perms/lib to system/proprietary/vzw/ instead of system.
    * The releasetools will move ../../ if device is vzw variant
    
    Change-Id: I7711bc19e717cce315f3dccacd34ad1b7b129ca6
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- configs/vzw.mk
+++ configs/vzw.mk
@@ -1,12 +1,21 @@
-#Permissions and Lib for Verizon Wireless Apps
 PRODUCT_PACKAGES += \
     VerizonSSO \
     CellBroadcastReceiver
 
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
-    vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
+# Copy permissions and lib to particular pkg based on if unified device
+# GMS ClientId base will be set as propert for vzw variant of unified device
+ifeq ($(TARGET_UNIFIED_DEVICE),true)
+    PRODUCT_COPY_FILES += \
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/proprietary/vzw/etc/permissions/com.vzw.har$
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/proprietary/vzw/etc/permissions/com.vzw.hardw$
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/proprietary/vzw/etc/permissions/com.vzw.vzwapnli$
+        vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/proprietary/vzw/lib/libmotricity.so
+else
+    PRODUCT_COPY_FILES += \
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+        vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+        vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
 
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
+    PRODUCT_GMS_CLIENTID_BASE := android-verizon
+endif
