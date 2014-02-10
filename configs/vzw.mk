2716ad2d6bbb10574326de6fbed3bf9a3d0b4120
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-02-10 23:49:40 +0100

    vzw: Add logic to vzw makefile for TARGET_UNIFIED_DEVICE
    * If target is a unified device, move the perms/lib to system/proprietary/vzw/ instead of system.
    * The releasetools will move ../../ if device is vzw variant
    
    Change-Id: I8f83143f334a26984503351e4779f24e7ba13a4d

--- configs/vzw.mk
+++ configs/vzw.mk
@@ -1,7 +1,16 @@
-#Permissions and Lib for Verizon Wireless Apps
+# Copy permissions and lib to particular pkg based on if unified device
+# GMS ClientId base will be set as propert for vzw variant of unified device
+
+ifeq ($(UNIFIED_DEVICE),true)
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/vzw/app/VerizonSSO.apk:system/proprietary/vzw/app/VerizonSSO.apk \
+    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/blobs/vzw/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/blobs/vzw/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/blobs/vzw/lib/libmotricity.so
+else
 PRODUCT_PACKAGES += \
-    VerizonSSO \
-    CellBroadcastReceiver
+    VerizonSSO
 
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
@@ -10,3 +19,4 @@ PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
+endif
