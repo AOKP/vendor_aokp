4aac82c7a71c6e3d04e20be60fd6a6fa15f4c925
Zaphod <mwilson@acesappraisal.com>
2014-02-22 10:14:13 -0500

    Add V500 (LG GTab 8.3)
    Change-Id: I54ffa9844fbe7147ae72f167b7af5455da84684c
    Signed-off-by: Zaphod <mwilson@acesappraisal.com>

---
+++ products/v500.mk
@@ -0,0 +1,23 @@
+# Inherit AOKP common_tablet bits
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/v500/full_v500.mk)
+
+# V500 Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/v500
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := v500
+PRODUCT_NAME := aokp_v500
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-V500
+PRODUCT_MANUFACTURER := lge
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080_1920.zip:system/media/bootanimation-alt.zip
+
