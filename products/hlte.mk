dbe5886200b8004595b9eb1c2c1a622c601f4ff5
Zach <zachf714@gmail.com>
2014-04-06 18:35:38 -0400

    Verizon device permissions for unified devices
    these perms go into /system/vzw/ and get extracted as normal if verizon
    
    Fix jfltevzw and d2vzw permissions after they were unified
    
    Patchset 2: Add jfLTE
    
    Patchset 3: Add HLTE, device side is not currently ready
    
    Change-Id: Ia92f72530bb58a92f21314943d0ef8f1c192b004

--- products/hlte.mk
+++ products/hlte.mk
@@ -6,6 +6,12 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
+# Unified device flag for Verizon permissions
+VZW_UNIFIED := true
+
+# Inherit common Verizon Wireless Perms and Lib
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
+
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := aokp_hlte
 
--- products/jflte.mk
+++ products/jflte.mk
@@ -6,6 +6,12 @@ $(call inherit-product, vendor/aokp/configs/gsm.mk)
 # Inherit common product files.
 $(call inherit-product, vendor/aokp/configs/common.mk)
 
+#Unified device flag for Verizon permissions
+VZW_UNIFIED := true
+
+# Inherit common Verizon Wireless perms & lib
+$(call inherit-product, vendor/aokp/configs/vzw.mk)
+
 PRODUCT_DEVICE := jflte
 PRODUCT_NAME := aokp_jflte
 
