aacce2b961a59f10cf0e643aa8c73b8a1efb43a3
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2014-04-13 14:21:37 +0200

    add back tilapia
    Change-Id: I495eefb61eb071dea2b6d1f88f00b0d9510d91f3
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ add_lunch_combo aokp_maguro-userdebug
 add_lunch_combo aokp_toro-userdebug
 add_lunch_combo aokp_manta-userdebug
 add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_tilapia-userdebug
 add_lunch_combo aokp_d2lte-userdebug
 add_lunch_combo aokp_d800-userdebug
 add_lunch_combo aokp_d801-userdebug
