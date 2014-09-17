1690b9ea2fa0ed8ec3fad2f9defee482b07e863c
kecinzer <kecinzer@gmail.com>
2014-09-17 20:01:45 +0200

    update fingerprint and build description from 10.5.A.0.230
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- products/yuga.mk
+++ products/yuga.mk
@@ -11,6 +11,8 @@ PRODUCT_DEVICE := yuga
 PRODUCT_MODEL := Xperia Z
 PRODUCT_MANUFACTURER := Sony
 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603_1270-6697/C6603:4.4.2/10.5.A.0.230/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="yuga-userdebug 4.4.2 10.5.A.0.230 WP5_rg test-keys"
+
 # boot animation
 PRODUCT_COPY_FILES += \
     vendor/aokp/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
