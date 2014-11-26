890fe679c3b228adf6eecd6a726f2d62293aea9e
Will DeBerry <willdeberry@gmail.com>
2014-11-26 19:59:24 +0100

    Update sepolicy for 5.0
    Change-Id: I159ee52799d364a3717f3d8b129dda1fe6b5678c

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,14 +1,22 @@
+#
+# This policy configuration will be used by all products that
+# inherit from AOKP
+#
+
 BOARD_SEPOLICY_DIRS += \
     vendor/aokp/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
-    fs_use \
     genfs_contexts \
     seapp_contexts \
+    service_contexts \
+    auditd.te \
     installd.te \
     netd.te \
+    sysinit.te \
+    system.te \
     ueventd.te \
     vold.te \
     mac_permissions.xml
