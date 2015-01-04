ebbd728d1280e9eb13981bc3dec43b1fadb12d45
Arnav Gupta <championswimmer@gmail.com>
2015-01-04 15:46:40 +0530

    aokp: update sepolicy
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,20 +1,28 @@
 #
 # This policy configuration will be used by all products that
-# inherit from AOKP
+# inherit from CM
 #
 
 BOARD_SEPOLICY_DIRS += \
-    vendor/aokp/sepolicy
+    vendor/cm/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     file.te \
     file_contexts \
     genfs_contexts \
+    property_contexts \
     seapp_contexts \
     service_contexts \
     auditd.te \
+    adbd.te \
+    healthd.te \
+    hostapd.te \
     installd.te \
     netd.te \
+    property.te \
+    recovery.te \
+    shell.te \
+    su.te \
     sysinit.te \
     system.te \
     ueventd.te \
