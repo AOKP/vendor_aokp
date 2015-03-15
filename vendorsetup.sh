8384d694eaec04668f0126ee42d7b2b2c33c7b55
Will DeBerry <willdeberry@gmail.com>
2015-03-15 23:57:38 +0100

    vendorsetup: move from device tree and reorder list
    Change-Id: I540dc419b405b328f3b04fd6fad602d2a4a3080d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,11 @@
 add_lunch_combo aokp_mako-userdebug
 add_lunch_combo aokp_hammerhead-userdebug
+add_lunch_combo aokp_grouper-userdebug
+add_lunch_combo aokp_amami-userdebug
+add_lunch_combo aokp_find7-userdebug
+add_lunch_combo aokp_find7s-userdebug
+add_lunch_combo aokp_honami-userdebug
 add_lunch_combo aokp_jfltevzw-userdebug
 add_lunch_combo aokp_shamu-userdebug
-add_lunch_combo aokp_yuga-userdebug
-add_lunch_combo aokp_honami-userdebug
 add_lunch_combo aokp_sirius-userdebug
+add_lunch_combo aokp_yuga-userdebug
