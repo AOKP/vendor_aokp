474a2b939d4c53da29ee0de22ebcb633203032cf
Arnav Gupta <championswimmer@gmail.com>
2016-02-09 16:17:49 +0000

    config: add TW_THEME default to let TWRP build
    Change-Id: I375c9d08ef3394b3d498a6b09fa19c51c6cb7b95
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- configs/common_full_phone.mk
+++ configs/common_full_phone.mk
@@ -6,4 +6,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Hassium.ogg
 
+#Set a default TWRP theme so that it builds
+TW_THEME := portrait_hdpi
+
 $(call inherit-product, vendor/aokp/configs/telephony.mk)
