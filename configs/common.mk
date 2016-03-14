024575501c23e72afd3c27f2ff9f5fdf78b6e00b
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-14 12:45:43 -0800

    Bring back Exchange2
    Dropped out via r22, but we want this
    
    Change-Id: Iec52790d7c69594d1101d9062fb649a3aca866ab

--- configs/common.mk
+++ configs/common.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
     LockClock \
     Trebuchet
 
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
 # CM Platform Library
 PRODUCT_PACKAGES += \
     org.cyanogenmod.platform-res \
