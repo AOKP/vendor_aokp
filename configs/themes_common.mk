9e448f317ae14d33c8fd7150242b6f8fb84e7167
Adnan Begovic <adnan@cyngn.com>
2016-03-15 14:17:47 -0700

    cm: Commonize cmsdk specific targets.
    Change-Id: I5e2f0b84ed34244b7549daf91053258193dd716f

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -3,6 +3,3 @@ PRODUCT_PACKAGES += \
     aapt \
     ThemeChooser \
     ThemesProvider
-
-PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/common/etc/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
