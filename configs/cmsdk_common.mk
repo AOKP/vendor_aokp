de30c2033f8d0b1654aa5f430c77c27f424277ee
Steve Kondik <steve@cyngn.com>
2016-03-31 11:24:53 -0700

    cm: Moving LiveDisplay to CMSDK
     * Also alphabetized the list of feature xmls, you filthy pigs.
    
    Change-Id: I094a46c313be4531c6dd1af1e007a26b2476d60e

--- configs/cmsdk_common.mk
+++ configs/cmsdk_common.mk
@@ -1,6 +1,7 @@
 # Permissions for cmsdk services
 PRODUCT_COPY_FILES += \
     vendor/aokp/configs/permissions/org.cyanogenmod.appsuggest.xml:system/etc/permissions/org.cyanogenmod.appsuggest.xml \
+    vendor/aokp/configs/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.livelockscreen.xml:system/etc/permissions/org.cyanogenmod.livelockscreen.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
