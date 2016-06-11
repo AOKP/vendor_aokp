namdeptrai2003/android_vendor_dnd

config: Make product size available for use by project mkfiles
This allows projects to opt out for space limited devices.

Also create a common_mini.mk and populate it with common stuff from
the common_mini_*.mk files.

Change-Id: I7d7dee73b70f97a778353ca136434a6e36edaf7e

config/common_mini.mk
@@ -1,0 +1,13 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/cm/config/common.mk)
+
+PRODUCT_SIZE := mini
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+

config/common_mini_phone.mk
@@ -1,17 +1,8 @@
-# Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
-
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+$(call inherit-product, vendor/cm/config/common_mini.mk)
 
 # Required CM packages
 PRODUCT_PACKAGES += \
     LatinIME
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \

config/common_mini_tablet_wifionly.mk
@@ -1,17 +1,9 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
-
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
+$(call inherit-product, vendor/cm/config/common_mini.mk)
 
 # Required CM packages
 PRODUCT_PACKAGES += \
     LatinIME
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \

config/common_mini_tv.mk
@@ -1,13 +1,5 @@
 # Inherit common CM stuff
-$(call inherit-product, vendor/cm/config/common.mk)
-
-# Include CM audio files
-include vendor/cm/config/cm_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
+$(call inherit-product, vendor/cm/config/common_mini.mk)
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \

AOKP/vendor_aokp

configs/common_mini.mk
@@ -1,0 +1,13 @@
+# Inherit common CM stuff
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+PRODUCT_SIZE := mini
+
+# Include CM audio files
+include vendor/aokp/configs/cm_audio.mk
+
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+

configs/common_mini_phone.mk
@@ -1,12 +1,3 @@
-# Inherit common AOKP stuff
-$(call inherit-product, vendor/aokp/configs/common.mk)
-
-# Include AOKP audio files
-include vendor/aokp/configs/cm_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+$(call inherit-product, vendor/aokp/configs/common_mini.mk)
 
 $(call inherit-product, vendor/aokp/configs/telephony.mk)

configs/common_mini_tablet_wifionly.mk
@@ -1,10 +1,2 @@
 # Inherit common AOKP stuff
-$(call inherit-product, vendor/aokp/configs/common.mk)
-
-# Include AOKP audio files
-include vendor/aokp/config/cm_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+$(call inherit-product, vendor/aokp/configs/common_mini.mk)

configs/common_mini_tv.mk
@@ -1,10 +1,2 @@
 # Inherit common AOKP stuff
-$(call inherit-product, vendor/aokp/config/common.mk)
-
-# Include CM audio files
-include vendor/aokp/config/cm_audio.mk
-
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
+$(call inherit-product, vendor/aokp/configs/common_mini.mk)