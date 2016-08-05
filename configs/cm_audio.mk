971c7f932726ce985d1c3a66e7e68900b953c149
task650 <task650@gmail.com>
2016-08-05 06:32:29 -0700

    Fix a little booger *Let yukaay join the ringtone party
    Change-Id: I53dc1ade00f038146af59b78c887c305a8709312

--- configs/cm_audio.mk
+++ configs/cm_audio.mk
@@ -38,6 +38,6 @@ PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/Highscore.ogg:system/media/audio/ringtones/Highscore.ogg \
     $(RINGTONE_PATH)/Lyon.ogg:system/media/audio/ringtones/Lyon.ogg \
     $(RINGTONE_PATH)/Rockin.ogg:system/media/audio/ringtones/Rockin.ogg \
-    $(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3
+    $(RINGTONE_PATH)/Sheep.mp3:system/media/audio/ringtones/Sheep.mp3 \
     $(RINGTONE_PATH)/Yukaay.ogg:system/media/audio/ringtones/Yukaay.ogg
 endif
