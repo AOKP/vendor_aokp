a1d86099a8e3ee59f48fdeffcd7586f11ab4dde8
Arne Coucheron <arco68@gmail.com>
2016-09-23 15:32:33 -0700

    cm: Minor config reorganization
     * Move extra tools to common.mk. These are needed for CMFM, and should
       be included for all.
     * Move PhotoTable, LiveWallpapersPicker, SoundRecorder and Screencast
       to common.mk. These apps are extremely small, and only takes up
       less than 1 MB combined.
     * Remove obsolete live wallpapers.
     * Move sound property overrides to common.mk. There's no need
       to have them spread across several files.
     * Include CM audio files from common.mk
    
    Change-Id: Ic1a22b0aeae8d36a591fd1890501ca2deb24cdb5

--- configs/common_full_phone.mk
+++ configs/common_full_phone.mk
@@ -1,9 +1,4 @@
 # Inherit common AOKP stuff
 $(call inherit-product, vendor/aokp/configs/common_full.mk)
 
-# Default notification/alarm sounds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg
-
 $(call inherit-product, vendor/aokp/configs/telephony.mk)
