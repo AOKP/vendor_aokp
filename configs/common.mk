d58063a4e004652eeaa30882eb79038352a33513
Arne Coucheron <arco68@gmail.com>
2016-09-23 15:32:33 -0700

    cm: Minor config reorganization  * Move extra tools to common.mk. These are needed for CMFM, and should    be included for all.  * Move PhotoTable, LiveWallpapersPicker, SoundRecorder and Screencast    to common.mk. These apps are extremely small, and only takes up    less than 1 MB combined.  * Remove obsolete live wallpapers.  * Move sound property overrides to common.mk. There's no need    to have them spread across several files.  * Include CM audio files from common.mk
    Change-Id: Ic1a22b0aeae8d36a591fd1890501ca2deb24cdb5

--- configs/common.mk
+++ configs/common.mk
@@ -60,6 +60,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
+# Default notification/alarm sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Argon.ogg \
+    ro.config.alarm_alert=Helium.ogg
+
 ifneq ($(TARGET_BUILD_VARIANT),user)
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
@@ -128,6 +133,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aokp/configs/permissions/com.aokp.android.xml:system/etc/permissions/com.aokp.android.xml
 
+# Include CM audio files
+include vendor/aokp/configs/cm_audio.mk
+
 # Theme engine
 include vendor/aokp/configs/themes_common.mk
 
@@ -174,7 +182,9 @@ PRODUCT_PACKAGES += \
     LockClock \
     Trebuchet \
     WeatherManagerService \
-    WeatherProvider
+    WeatherProvider \
+    SoundRecorder \
+    Screencast
 
 # Exchange support
 PRODUCT_PACKAGES += \
@@ -195,7 +205,18 @@ PRODUCT_PACKAGES += \
     oprofiled \
     sqlite3 \
     strace \
-    pigz
+    pigz \
+    7z \
+    lib7z \
+    bash \
+    bzip2 \
+    curl \
+    powertop \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
 
 # Custom off-mode charger
 ifneq ($(WITH_CM_CHARGER),false)
