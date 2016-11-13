05dd8f529cde624253adacaca8a8177365a55c94
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-13 10:08:08 -0800

    config: Add missing \ in bootanimation pick
    Change-Id: Id61a196f97115c70b7d1dcf84fe830b72834a17c

--- configs/common.mk
+++ configs/common.mk
@@ -18,7 +18,7 @@ bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_siz
 # find the appropriate size and set
 define check_and_set_bootanimation
 $(eval TARGET_BOOTANIMATION_NAME := $(shell \
-  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then \
     if [ "$(1)" -le "$(TARGET_BOOTANIMATION_SIZE)" ]; then \
       echo $(1); \
       exit 0; \
@@ -306,7 +306,7 @@ ifneq ($(AOKP_BUILD),)
 else
     ifeq ($(AOKP_BUILDTYPE),)
         # AOKP_BUILDTYPE not defined
-	AOKP_BUILDTYPE := unofficial
+	AOKP_BUILDTYPE := official
     endif
 
     AOKP_VERSION=$(TARGET_PRODUCT)_$(AOKP_BRANCH)_$(AOKP_BUILDTYPE)_$(DATE)
