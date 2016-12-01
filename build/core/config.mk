8d84a33e354839e8569703f61522199100e39b36
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-11-30 18:56:46 -0600

    vendor: Add a task for getting prebuilts via http
     * Gello being our first pawn
     * Use curl instead of wget (more powerful)
     * Ensure the intermediates directory exists prior to download
    
    Change-Id: If19c18638a14a8b9ab55470ae6ee5a2deda2e3ec

--- build/core/config.mk
+++ build/core/config.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2015 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,3 +27,5 @@ FRAMEWORK_CM_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/aokp/build/core/apicheck_m
 
 BUILD_MAVEN_PREBUILT := $(TOP)/vendor/aokp/build/core/maven_artifact.mk
 PUBLISH_MAVEN_PREBUILT := $(TOP)/vendor/aokp/build/core/maven_artifact_publish.mk
+
+BUILD_HTTP_PREBUILT := $(TOP)/vendor/aokp/build/core/http_prebuilt.mk
