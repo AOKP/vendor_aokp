b5b9e66c49fdd101f823d5592c3d49ea95ff0a47
Michael Bestas <mkbestas@lineageos.org>
2017-08-20 15:42:23 +0300

    cm: Remove theme engine leftovers
    Change-Id: Ife3fa6d6bfb5994fa120a959e6c5ba1faf6b2416

--- configs/cmsdk_common.mk
+++ configs/cmsdk_common.mk
@@ -9,7 +9,6 @@ PRODUCT_COPY_FILES += \
     vendor/aokp/configs/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.telephony.xml:system/etc/permissions/org.cyanogenmod.telephony.xml \
-    vendor/aokp/configs/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.weather.xml:system/etc/permissions/org.cyanogenmod.weather.xml
 
 # CM Platform Library
