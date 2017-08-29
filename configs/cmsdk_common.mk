0cf34d7ad38589ea68d44c00ec7e4c1b8f003807
Michael Bestas <mkbestas@lineageos.org>
2017-08-29 19:09:06 +0300

    cm: Remove partner interface
    Change-Id: I22ad161f4587703b7f3c2919890a19e69c1a8e0f

--- configs/cmsdk_common.mk
+++ configs/cmsdk_common.mk
@@ -2,7 +2,6 @@
 PRODUCT_COPY_FILES += \
     vendor/aokp/configs/permissions/org.cyanogenmod.audio.xml:system/etc/permissions/org.cyanogenmod.audio.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.livedisplay.xml:system/etc/permissions/org.cyanogenmod.livedisplay.xml \
-    vendor/aokp/configs/permissions/org.cyanogenmod.partner.xml:system/etc/permissions/org.cyanogenmod.partner.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.performance.xml:system/etc/permissions/org.cyanogenmod.performance.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.profiles.xml:system/etc/permissions/org.cyanogenmod.profiles.xml \
     vendor/aokp/configs/permissions/org.cyanogenmod.statusbar.xml:system/etc/permissions/org.cyanogenmod.statusbar.xml \
