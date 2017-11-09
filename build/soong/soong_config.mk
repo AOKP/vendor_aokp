42f29543059201e298b5f8df6fcbb79c59137b5e
STELIX <ssspinni@gmail.com>
2017-11-09 16:50:30 +0100

    vendor_aokp - Initial Oreo bring-up
    Change-Id: Ic5dee75822f5db93d24423de9afef00bdbad535a
    Signed-off-by: STELIX <ssspinni@gmail.com>

---
+++ build/soong/soong_config.mk
@@ -0,0 +1,9 @@
+# Insert new variables inside the AOKP structure
+lineage_soong:
+	$(hide) mkdir -p $(dir $@)
+	$(hide) (\
+	echo '{'; \
+	echo '"Lineage": {'; \
+	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)'; \
+	echo '},'; \
+	echo '') > $(SOONG_VARIABLES_TMP)
