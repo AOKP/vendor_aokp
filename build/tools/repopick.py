8697094d56a432918272c4ec7586165de256fc01
STELIX <ssspinni@gmail.com>
2017-11-16 18:53:27 +0100

    vendor/aokp - some more LOS/AOKPization
    Change-Id: If98d401f8592e5903a4b9e7ec80b6653b195cc21
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- build/tools/repopick.py
+++ build/tools/repopick.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2013-15 The CyanogenMod Project
 #           (C) 2017    The LineageOS Project
+#           (C) 2017    The AOKP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
