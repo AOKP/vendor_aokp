8b39c80f102ae65ffa42161cc240563416a06417
Aaron Kling <webgeek1234@gmail.com>
2017-12-07 13:55:52 -0600

    ATV: Ship AppDrawer
    Also remove TvSettings from here since its included in atv_base
    
    Change-Id: I2d97e90e234dc9b7868dad3b063c0420211dacdc

--- configs/common_full_tv.mk
+++ configs/common_full_tv.mk
@@ -1,6 +1,6 @@
 # Inherit common AOKP stuff
 $(call inherit-product, vendor/aokp/config/common_full.mk)
 
-PRODUCT_PACKAGES += TvSettings
+PRODUCT_PACKAGES += AppDrawer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/aokp/overlay/tv
