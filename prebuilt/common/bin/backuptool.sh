LineageOS/android_vendor_lineage

backuptool: Temporarily render version check permissive
 * Due to both following commits, backuptool went permissive
    and lineage properties got lost from the system on devices
    that do not have BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED

    "backuptool: Take into account new location for system default props"
    Change-Id: I62046447876c2198a0c4f88a4f36f4723d417617

    "lineage: Move to Google's method of defining system default props"
    Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b

 * Once BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED is globally enabled,
    a period of time is required for "most" of the users to upgrade
    their system with fixed lineage properties before breaking addons
    by repairing the backuptool script

Change-Id: Iea8865ea9bb05eed56a8a0a7b95e3f04b01c4bae

prebuilt/common/bin/backuptool.sh
@@ -34,10 +34,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-if [ ! grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-  echo "Not backing up files from incompatible version: $V"
-  return 0
-fi
+# if [ ! grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+#   echo "Not backing up files from incompatible version: $V"
+#   return 0
+# fi
 return 1
 }
 

AOKP/vendor_aokp

 * Once the properties issue is properly resolved in the sources,
    their system with fixed lineage properties before we break addons
    by repairing the backuptool script globally

prebuilt/common/bin/backuptool.sh
@@ -34,10 +34,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-if [ ! grep -q "^ro.aokp.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-  echo "Not backing up files from incompatible version: $V"
-  return 0
-fi
+# if [ ! grep -q "^ro.aokp.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+#   echo "Not backing up files from incompatible version: $V"
+#   return 0
+# fi
 return 1
 }
 