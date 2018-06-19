2a1f51b712be05db9e34ecf2a96759e3d5cbf195
Aaron Kling <webgeek1234@gmail.com>
2018-06-19 12:10:10 -0500

    ATV: Build LineageCustomizer
    Change-Id: I1a9e18516441dd513378bc5332b181b4e059e19b

--- configs/common_full_tv.mk
+++ configs/common_full_tv.mk
@@ -1,6 +1,8 @@
 # Inherit common AOKP stuff
 $(call inherit-product, vendor/aokp/config/common_full.mk)
 
-PRODUCT_PACKAGES += AppDrawer
+PRODUCT_PACKAGES += \
+    AppDrawer \
+    LineageCustomizer
 
 DEVICE_PACKAGE_OVERLAYS += vendor/aokp/overlay/tv
