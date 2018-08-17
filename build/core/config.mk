aa1e593dc8edfcb397cad025aa7c2856a7f09df0
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    aokp: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a

--- build/core/config.mk
+++ build/core/config.mk
@@ -22,6 +22,11 @@ FRAMEWORK_LINEAGE_API_NEEDS_UPDATE_TEXT := $(TOPDIR)vendor/aokp/build/core/apich
 
 BUILD_RRO_SYSTEM_PACKAGE := $(TOPDIR)vendor/aokp/build/core/system_rro.mk
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
 # Rules for MTK targets
 include $(TOPDIR)vendor/aokp/build/core/mtk_target.mk
 
