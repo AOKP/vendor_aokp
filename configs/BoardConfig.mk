8bb7070e3386aed51ece15b2f56526023b24249d
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-18 21:53:24 -0700

    aokp: Dynamically generate kernel headers using lineage generator
    Add a soong vendor plugin for kernel config variables so we can use
    these in go.
    
    Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

--- configs/BoardConfig.mk
+++ configs/BoardConfig.mk
@@ -8,3 +8,5 @@ include vendor/aokp/configs/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/aokp/configs/BoardConfigQcom.mk
 endif
+
+include vendor/aokp/configs/BoardConfigSoong.mk
