e4245647f14fe59697275323735e44f1ebde3553
villarleg <santialovera20@gmail.com>
2018-09-25 17:28:04 +0200

    soong: Rename variables

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,6 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "Lineage":{$(newline)
+_contents := $(_contents)    "Aokp":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
