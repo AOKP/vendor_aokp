8bb7ee187f81f2f04172fed40a77f76f3a74096e
Aaron Kling <webgeek1234@gmail.com>
2019-08-30 20:44:20 -0500

    Disable AudioFX on atv
    The interface is not usable on atv
    
    Change-Id: Ibe2c33e5528b593ae338a62cee62e6fe9cfe47ad

--- configs/common_full_tv.mk
+++ configs/common_full_tv.mk
@@ -1,5 +1,9 @@
+# Exclude AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Inherit common AOKP stuff
 $(call inherit-product, vendor/aokp/config/common_full.mk)
+
 
 # Inherit Lineage atv device tree
 $(call inherit-product, device/lineage/atv/lineage_atv.mk)
