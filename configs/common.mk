da8686922970af8ad074019e32f0b91f8184855f
Eric Clothier <eric.clothier@gmail.com>
2019-12-29 14:10:46 -0500

    vendor/aokp: configs/common.mk: fix whitespace on ro.control_privapp_permissions default prop define
    Fixes build fail due to extra back slash in default.prop
    
    Change-Id: I7c5dd88e2534210e62c493a6ec9e4328b697994e

--- configs/common.mk
+++ configs/common.mk
@@ -113,7 +113,7 @@ PRODUCT_COPY_FILES += \
     vendor/aokp/configs/permissions/com.aokp.android.xml:system/etc/permissions/com.aokp.android.xml
 
 # Enforce privapp-permissions whitelist	
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \	
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
 # Hidden API whitelist
